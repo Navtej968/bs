3cb35342c88439b2ea6bc98eddd7897b2ac6e5c5
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2024-11-27 09:49:08 +0530

    Update main.py
    uses pollintaion ai now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from get_prompt import get_prompt, style_list
 from keep_alive import keep_alive
+import urllib.request
 #from typing import BinaryIO
 from PIL import Image
 import requests
@@ -16,40 +17,44 @@ from ensta import Host
 
 
 def login():
-  insta = Host(os.environ['username'], os.environ['password'])
+  # insta = Host(os.environ['username'], os.environ['password'])
+  insta = Host("automated_art" , "69xdebugger")
   return insta
   
   
-def getimage():
-  engine_id = "stable-diffusion-v1-6"
-  response = requests.post(
-    f"https://api.stability.ai/v1/generation/{engine_id}/text-to-image",
-    headers={
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-      "Authorization": "sk-LZ9mrQgoLzIQYKmbZhj6GBJipSYHcJAzPgZhRo6wDpoK5v8A"
-    },
-    json={
-      "text_prompts": [{
-        "text": get_prompt()
-      }],
-      "cfg_scale": 7,
-      "clip_guidance_preset": "FAST_BLUE",
-      "height": 512,
-      "width": 512,
-      "style_preser": style_list[random.randrange(0, 16)],
-      "samples": 1,
-      "steps": 30,
-    })
-  data = response.json()
-  byte = (data['artifacts'][0]['base64'])
-  decoded = base64.b64decode(byte)
-  img = Image.open(io.BytesIO(decoded))
-  img.save('pic.jpg')
+# def getimage():
+#   engine_id = "stable-diffusion-v1-6"
+#   response = requests.post(
+#     f"https://api.stability.ai/v1/generation/{engine_id}/text-to-image",
+#     headers={
+#       "Content-Type": "application/json",
+#       "Accept": "application/json",
+#       "Authorization": "sk-LZ9mrQgoLzIQYKmbZhj6GBJipSYHcJAzPgZhRo6wDpoK5v8A"
+#     },
+#     json={
+#       "text_prompts": [{
+#         "text": get_prompt()
+#       }],
+#       "cfg_scale": 7,
+#       "clip_guidance_preset": "FAST_BLUE",
+#       "height": 512,
+#       "width": 512,
+#       "style_preser": style_list[random.randrange(0, 16)],
+#       "samples": 1,
+#       "steps": 30,
+#     })
+#   data = response.json()
+#   byte = (data['artifacts'][0]['base64'])
+#   decoded = base64.b64decode(byte)
+#   img = Image.open(io.BytesIO(decoded))
+#   img.save('pic.jpg')
 
+def get_img():
+  prompt = get_prompt()
+  urllib.request.urlretrieve(f'https://image.pollinations.ai/prompt/{prompt}', 'pic.png')
 
 def upload_img(insta):
-  upload_id = insta.get_upload_id("pic.jpg")
+  upload_id = insta.get_upload_id("pic.png")
   insta.upload_photo(upload_id,"pewpewpew \n #art#aiart#idk")
   os.remove("pic.jpg")
 
@@ -58,7 +63,7 @@ def main():
   keep_alive()
   while (True):
     insta = login()
-    getimage()
+    get_img()
     upload_img(insta)
     print(get_prompt())
     time.sleep(21600)
