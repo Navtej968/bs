40e373465493cd7c286767ad084cacec1573f3eb
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2024-11-30 12:38:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from get_prompt import get_prompt, style_list
 from keep_alive import keep_alive
-#from typing import BinaryIO
+import urllib
 from PIL import Image
 import requests
 import json
@@ -9,49 +9,23 @@ import base64
 import time
 import io
 import os
-#from insta_share import Instagram
-from ensta import Mobile
-
-#insta = Host()
+from instagrapi import Client
 
 
 def login():
-  mobile = Mobile(os.environ['username'], os.environ['password'])
-  return mobile
+  host = Client()
+  host.login(os.environ['username'], os.environ['password'])
+  return host
   
   
 def getimage():
-  engine_id = "stable-diffusion-v1-6"
-  response = requests.post(
-    f"https://api.stability.ai/v1/generation/{engine_id}/text-to-image",
-    headers={
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-      "Authorization": "sk-LZ9mrQgoLzIQYKmbZhj6GBJipSYHcJAzPgZhRo6wDpoK5v8A"
-    },
-    json={
-      "text_prompts": [{
-        "text": get_prompt()
-      }],
-      "cfg_scale": 7,
-      "clip_guidance_preset": "FAST_BLUE",
-      "height": 512,
-      "width": 512,
-      "style_preser": style_list[random.randrange(0, 16)],
-      "samples": 1,
-      "steps": 30,
-    })
-  data = response.json()
-  byte = (data['artifacts'][0]['base64'])
-  decoded = base64.b64decode(byte)
-  img = Image.open(io.BytesIO(decoded))
-  img.save('pic.jpg')
+  prompt = get_prompt().replace(' ','-')
+  urllib.request.urlretrieve(f'https://image.pollinations.ai/prompt/{prompt}', 'pic.jpg')
 
 
 def upload_img(insta):
-  upload_id = insta.get_upload_id("pic.png")
-  insta.upload_photo(upload_id,"pewpewpew \n #art#aiart#idk")
-  os.remove("pic.png")
+  insta.photo_upload("pic.jpg","pewpewpew \n #art#aiart#idk")
+  os.remove("pic.jpg")
 
 
 def main():
